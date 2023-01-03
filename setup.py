cbe0e3a5646d5fa3026cc54341484fe837bf085b
zero521 <happyzero521@163.com>
2023-01-03 08:54:05 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pythonProject',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='zeroye',
+    author_email='',
+    description=''
+)
