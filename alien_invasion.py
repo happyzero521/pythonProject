9eee7ed0170729e6c33c6ab11215dcf7242bcf25
zero <450026006@qq.com>
2023-01-17 00:13:34 +0800

    set aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,14 +14,15 @@ def run_game():
     pygame.display.set_caption("外星人入侵")
     # 创建飞船
     ship = Ship(ai_settings, screen)
-    alien = Alien(ai_settings, screen)
     bullets = Group()
+    aliens = Group()
+    gf.create_fleet(ai_settings, screen, ship, aliens)
     # 设置游戏主循环
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship,alien, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 # 启动游戏程序
